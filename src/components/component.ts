5220227433ba1f4260257f1b1f60ebf4ab2ca84a
sohyeonBak <sohyeonbakoly@gmail.com>
2021-07-22 20:43:11 +0900

    drag and drop

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,6 +1,8 @@
+
 export interface Component {
   attachTo(parent: HTMLElement, position?: InsertPosition): void;
   removeFrom(parent: HTMLElement): void;
+  attach(component: Component, position?: InsertPosition): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
@@ -21,5 +23,9 @@ export class BaseComponent<T extends HTMLElement> implements Component {
       throw new Error('Parent mist')
     }
     parent.removeChild(this.element)
+  }
+
+  attach(component: Component, position: InsertPosition = 'afterbegin') {
+    component.attachTo(this.element, position)
   }
 }
